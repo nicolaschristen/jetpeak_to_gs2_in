9ca1ffea689ba63d7d017ca9bfccf8455ab45d6f
nicolaschristen <nicolas.christen@physics.ox.ac.uk>
2020-05-13 12:06:37 +0100

    Add switch to toggle GS2 normalisation on/off in xaxis when plotting deposition profiles.

--- plot_heatDep.m
+++ plot_heatDep.m
@@ -2,14 +2,31 @@
 % The heat deposition is expressed in [W/m^3].
 %
 % Input :   ijp -- vector of JETPEAK indices
+%           nrm_gs2 -- [optional, =0] use GS2 normalisation
+%                      for x-axis
 %
 % Output:   -
 %
-function plot_heatDep(ijp)
+function plot_heatDep(ijp, varargin)
+
+
+% Read optional input arguments
+options_default = struct( 'nrm_gs2', 0 );
+opt = get_optargin(options_default, varargin);
+
+nrm_gs2 = opt.nrm_gs2;
 
 for ishot = 1:numel(ijp)
 
     jData = read_jData(ijp(ishot));
+
+    if nrm_gs2
+        xvar = jData.rpsi/jData.a;
+        xlab = '$r_\psi/a$';
+    else
+        xvar = jData.rpsi;
+        xlab = '$r_\psi$ [m]';
+    end
 
     if ~isempty(jData.idxAscot)
     
@@ -32,11 +49,11 @@ for ishot = 1:numel(ijp)
         color = get(h,'Color');
         if ~isnan(Si_PEN(1))
             hold on
-            plot(jData.rpsi/jData.a, Si_PEN, 'LineStyle', '--', 'Color', color)
+            plot(xvar, Si_PEN, 'LineStyle', '--', 'Color', color)
             lgd{end+1} = 'PENCIL';
         end
         grid on
-        xlabel('$r_\psi/a$')
+        xlabel(xlab)
         ylabel('$S_{Q,i}$ [W/$m^3$]')
         legend(lgd,'Location','SouthEast')
         title(ttl,'FontSize',16)
@@ -50,11 +67,11 @@ for ishot = 1:numel(ijp)
         color = get(h,'Color');
         if ~isnan(Se_PEN(1))
             hold on
-            plot(jData.rpsi/jData.a, Se_PEN, 'LineStyle', '--', 'Color', color)
+            plot(xvar, Se_PEN, 'LineStyle', '--', 'Color', color)
             lgd{end+1} = 'PENCIL';
         end
         grid on
-        xlabel('$r_\psi/a$')
+        xlabel(xlab)
         ylabel('$S_{Q,e}$ [W/$m^3$]')
         legend(lgd,'Location','SouthEast')
         title(ttl,'FontSize',16)
--- plot_torqueDep.m
+++ plot_torqueDep.m
@@ -2,14 +2,31 @@
 % The torque deposition is expressed in [Pa].
 %
 % Input :   ijp -- vector of JETPEAK indices
+%           nrm_gs2 -- [optional, =0] use GS2 normalisation
+%                      for x-axis
 %
 % Output:   -
 %
 function plot_torqueDep(ijp)
 
+
+% Read optional input arguments
+options_default = struct( 'nrm_gs2', 0 );
+opt = get_optargin(options_default, varargin);
+
+nrm_gs2 = opt.nrm_gs2;
+
 for ishot = 1:numel(ijp)
 
     jData = read_jData(ijp(ishot));
+
+    if nrm_gs2
+        xvar = jData.rpsi/jData.a;
+        xlab = '$r_\psi/a$';
+    else
+        xvar = jData.rpsi;
+        xlab = '$r_\psi$ [m]';
+    end
 
     if ~isempty(jData.idxAscot)
     
@@ -28,11 +45,11 @@ for ishot = 1:numel(ijp)
         color = get(h,'Color');
         if ~isnan(S_PEN(1))
             hold on
-            plot(jData.rpsi/jData.a, S_PEN, 'LineStyle', '--', 'Color', color)
+            plot(xvar, S_PEN, 'LineStyle', '--', 'Color', color)
             lgd{end+1} = 'PENCIL';
         end
         grid on
-        xlabel('$r_\psi/a$')
+        xlabel(xlab)
         ylabel('$S_{\Pi}$ [Pa]')
         legend(lgd,'Location','SouthEast')
         title(ttl,'FontSize',16)
