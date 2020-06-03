76aaf97aec835f53241a58f62421a3982d8ee065
nicolaschristen <nicolas.christen@physics.ox.ac.uk>
2020-06-03 11:26:58 +0100

    Return dimensionFUL quantities. Change normalisation of Pi to account for trinity_norm.

--- plot_momFlux.m
+++ plot_momFlux.m
@@ -2,16 +2,29 @@
 % The fluxes are expressed in GS2 units [nref*mref*vthref^2*Lref*rhostar^2].
 %
 % Input :   ijp -- JETPEAK index of shot
-%           ylim -- optional, ylim for plot
-%           jData -- optional, provide pre-read JETPEAK data
+%           ylim -- [kw, []] ylim for plot
+%           jData -- [kw, []] provide pre-read JETPEAK data
+%           gs2_fluxFile -- [kw, ''] file containing GS2 fluxes
+%                           to be plotted
+%           nrm_gs2 -- [kw, 0] normalise plotted quantities to GS2 units
+%           showTitle -- [kw, 1] add title to plots
+%           showAllCodes -- [kw, 0] plot other deposition codes than
+%                           ASCOT, eg PENCIL
+%           trinity_norm -- [kw, 0] if true, gs2 flux dotted with gradPsi
+%                           else dotted with grad(x).
 %
-% Output:   rhoc -- normalised radial coordinate
-%           PI_ASC_GS2 -- normalised momentum flux from ASCOT
+% Output:   r -- dimensionful radial coordinate
+%           PI_ASC -- momentum flux from ASCOT
 %
-function [rhoc, PI_ASC_GS2] = plot_momFlux(ijp, varargin)
+function [r, PI_ASC] = plot_momFlux(ijp, varargin)
 
 opt_defaults = struct( 'ylim', [], ...
-                       'jData', [] );
+                       'jData', [], ...
+                       'gs2_fluxFile', '', ...
+                       'nrm_gs2', 0, ...
+                       'showTitle', 1, ...
+                       'showAllCodes', 0, ...
+                       'trinity_norm', 0 );
 opt = get_optargin(opt_defaults, varargin);
 
 % Elementary charge
@@ -24,9 +37,16 @@ else
     jData = opt.jData;
 end
 
-rhoc = jData.rpsi/jData.a;
+r = jData.rpsi;
+    
+% Read GS2 fluxes form file
 
-%% Net ion heat flux [W]
+if ~isempty(opt.gs2_fluxFile)
+    flx = read_gs2Fluxes( ijp, opt.gs2_fluxFile, 'jData', jData, ...
+                          'trinity_norm', opt.trinity_norm );
+end
+
+%% Total momentum flux [W]
 
 figure
 lgd_h = [];
@@ -37,45 +57,91 @@ PI_PEN = jData.PI_PENCIL;
 PI_ASC = jData.PI_ASCOT;
 
 % GS2 normalisations
-PI_PEN_GS2 = PI_PEN ...
-    ./ (jData.nref.*jData.mref.*jData.vthref.^2) ...
-    .*(jData.a./(jData.rhoref)).^2.;
-PI_ASC_GS2 = PI_ASC ...
-    ./ (jData.nref.*jData.mref.*jData.vthref.^2) ...
-    .*(jData.a./(jData.rhoref)).^2.;
+if ~opt.trinity_norm
+    PINorm = jData.nref.*jData.mref.*jData.vthref.^2.*jData.a.*jData.rhostar.^2./jData.dx_dpsi;
+else
+    PINorm = jData.nref.*jData.mref.*jData.vthref.^2.*jData.a.*jData.rhostar.^2.*jData.gradPsiAvg;
+end
+PI_PEN_GS2 = PI_PEN./PINorm;
+PI_ASC_GS2 = PI_ASC./PINorm;
 
 % Plot
 
-if ~isnan(PI_PEN_GS2(1))
+if opt.nrm_gs2
+    xvar = jData.rpsi/jData.a;
+    xlab = '$r_\psi/a$';
+    ylab = '$\sum_s\Pi_s$ [$n_r$ $m_r$ $v_{thr}^2$ $\rho_\star^2$]';
+    yvar_PEN = PI_PEN_GS2;
+    yvar_ASC = PI_ASC_GS2;
+else
+    xvar = jData.rpsi;
+    xlab = '$r_\psi$ [m]';
+    ylab = '$\sum_s\Pi_s$ [kg/s$^2$]';
+    yvar_PEN = PI_PEN;
+    yvar_ASC = PI_ASC;
+end
 
-    lgd_h(end+1) = semilogy(rhoc, PI_PEN_GS2);
+if ~isnan(PI_PEN_GS2(1)) && opt.showAllCodes
+
+    lgd_h(end+1) = semilogy(xvar, yvar_PEN);
     hold on
-    lgd_txt{end+1} = 'PENCIL';
+    lgd_txt{end+1} = 'Experiment (PENCIL)';
     color = get(lgd_h(end), 'Color');
     alpha = 0.3;
-    confid_area(gcf, rhoc, PI_PEN_GS2*0.8, ...
-        PI_PEN_GS2*1.2, color, alpha)
+    confid_area(gcf, xvar, yvar_PEN*0.8, ...
+        yvar_PEN*1.2, color, alpha)
 
 end
 
-lgd_h(end+1) = semilogy(rhoc, PI_ASC_GS2);
+h = semilogy(xvar, yvar_ASC);
+lgd_h(end+1) = h;
+lgd_txt{end+1} = 'Experiment (ASCOT)';
+
+% Plot flux from  gs2 simulations
+
+if ~isempty(opt.gs2_fluxFile)
+    if opt.nrm_gs2
+        xvar = flx.rpsi/jData.a;
+        yvar = flx.PI_gs2;
+    else
+        xvar = flx.rpsi;
+        yvar = flx.PI_gs2.*flx.PINorm;
+    end
+    hold on
+    lgd_h(end+1) = semilogy(xvar, yvar, ...
+                            'Marker', '.', ...
+                            'MarkerSize', 20);
+    lgd_txt{end+1} = 'GS2';
+end
+
+% Add experimental confidence area
+
+if opt.nrm_gs2
+    xvar = jData.rpsi/jData.a;
+else
+    xvar = jData.rpsi;
+end
 hold on
-lgd_txt{end+1} = 'ASCOT';
-color = get(lgd_h(end), 'Color');
+color = get(h, 'Color');
 alpha = 0.3;
-confid_area(gcf, rhoc, PI_ASC_GS2*0.8, ...
-    PI_ASC_GS2*1.2, color, alpha)
+confid_area(gcf, xvar, yvar_ASC*0.8, ...
+    yvar_ASC*1.2, color, alpha)
 
 % Fine-tune figure
 
-ttl = ['shot=' num2str(jData.shot) ...
-    ', ijp=' num2str(jData.ijp) ...
-    ', idxAscot=' num2str(jData.idxAscot)];
-title(ttl, 'FontSize',16)
+if opt.showTitle
+    ttl = ['shot=' num2str(jData.shot) ...
+        ', ijp=' num2str(jData.ijp) ...
+        ', idxAscot=' num2str(jData.idxAscot)];
+    title(ttl, 'FontSize',16)
+end
+
 grid on
-xlabel('$r_\psi/a$')
-ylabel('$\sum_s\Pi_s \ \left[ n_r m_r v_{thr}^2 \rho_\star^2 \right]$')
-legend(lgd_h, lgd_txt, 'Location', 'NorthEast','FontSize',14)
+xlabel(xlab)
+ylabel(ylab)
+if opt.showAllCodes || ~isempty(opt.gs2_fluxFile)
+    legend(lgd_h, lgd_txt, 'Location', 'SouthEast','FontSize',14)
+end
 if ~isempty(opt.ylim)
     ylim(opt.ylim)
 end
